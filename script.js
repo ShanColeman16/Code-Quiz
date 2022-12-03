2bc38fccac194f236d04c92cde5ac67fcc48a9b3
unknown <shannoncoleman16@gmail.com>
2022-12-03 17:59:03 -0500

    added start button

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-// Added nextEl = Document.querySelector("#next") to move questions forward
+//GIVEN I am taking a code quiz
+//STEP: 1.
+// WHEN I click the start button
+var startButtonQuizEl = document.querySelector(".Start-Quiz");
+//THEN a timer starts and I am presented with a question
 
-var nextEl = Document.querySelector("#next");
+
+////STEP 2: 
+//WHEN I answer a question
+
+//THEN I am presented with another question
+
+
+////STEP 3: 
+//WHEN I answer a question incorrectly
+
+//THEN time is subtracted from the clock
+
+
+////STEP 4.: 
+//WHEN all questions are answered or the timer reaches 0
+
+
+//THEN the game is over
+
+
+////STEP 5.:
+// WHEN the game is over
+
+
+//THEN I can save my initials and my score
+
+
+
+// Added nextEl = Document.querySelector("#next") to move questions forward
+var nextEl = document.querySelector("#next");
+testNum = 5
 var pointer = 0;
 var questions = [
   "This is question one?", 
@@ -17,12 +51,11 @@ var displayQuestion = function () {
 };
 
 // Added advance function to advance screen forward
-var advance = function() {
+var renderAndStartTimer = function() {
   if (pointer < questions.length -1) {
     pointer++;
-  } else {
-    
-  }
+  } 
+
   displayQuestion();
 
 };
