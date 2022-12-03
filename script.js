407441d6e8a75233c4f101af858c06fb1964f2e4
unknown <shannoncoleman16@gmail.com>
2022-12-03 12:01:46 -0500

    added var and functions

---
+++ script.js
@@ -0,0 +1,20 @@
+var nextEl = Document.querySelector("#next");
+var pointer = 0;
+var questions = [
+  "This is question one?", 
+  "This is question two?",
+  "This is question three?",
+  "This is question four?" 
+
+];
+
+var displayQuestion = function () {
+
+
+};
+
+
+var advance = function() {
+
+
+};
