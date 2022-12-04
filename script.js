20c81bec1324732fb60a6f11f797f05d1b30396a
unknown <shannoncoleman16@gmail.com>
2022-12-04 18:00:43 -0500

    added timer function

--- script.js
+++ script.js
@@ -2,9 +2,30 @@
 //STEP: 1.
 // WHEN I click the start button
 var startButtonQuizEl = document.querySelector(".Start-Quiz");
+var timerId
 //THEN a timer starts and I am presented with a question
-
-
+var timeEl = document.getElementById("timer");
+var secondsLeft = 90;
+function displayQuestion() {
+  //nextEl.textContent =  questions[pointer];
+  console.log("displayQuestion");
+};
+function displayTime() {
+  secondsLeft--;
+  timeEl.textContent = secondsLeft;
+  if (secondsLeft <=0) {
+    endQuiz()
+  } 
+};
+function startQuiz() {
+  timerId = setInterval(displayTime, 1000)
+  timeEl.textContent = secondsLeft;
+  displayQuestion()
+};
+function endQuiz(){
+  console.log("endquiz");
+  clearInterval(timerId)
+}
 ////STEP 2: 
 //WHEN I answer a question
 
@@ -34,34 +55,29 @@ var startButtonQuizEl = document.querySelector(".Start-Quiz");
 
 // Added nextEl = Document.querySelector("#next") to move questions forward
 var nextEl = document.querySelector("#next");
-testNum = 5
 var pointer = 0;
 var questions = [
-  "This is question one?", 
+  "This is question one?",
   "This is question two?",
   "This is question three?",
-  "This is question four?" 
+  "This is question four?",
 
 ];
 
 // Added function to display questions
-var displayQuestion = function () {
-  nextEl.textContent =  questions[pointer];
 
-};
 
 // Added advance function to advance screen forward
-var renderAndStartTimer = function() {
-  if (pointer < questions.length -1) {
+var queryAndStartTimer = function () {
+  if (pointer < questions.length - 1) {
     pointer++;
-  } 
-
+  }
   displayQuestion();
 
 };
 
 // Added EventListener
-nextEl.addEventListener("click", advance);
-
+//nextEl.addEventListener("click", pointer);
+startButtonQuizEl.addEventListener("click", startQuiz);
 //Added to display the first question when page loads
-displayQuestion();
+//displayQuestion();
